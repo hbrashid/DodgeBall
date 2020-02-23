16330342b1b2d7d66aca025c6146fcd8f43f1957
hbrashid <hbrashid@gmail.com>
2020-02-23 13:57:13 -0600

    added tests and tweaks

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # DodgeBall
 JS211 - Checkpoint 2 - Dodge Ball
 
+** Don't forget to 'npm i' so the tests work in node **
+
 We are in need of a sorting and organizing app for our community dodge ball league. There are already 6 players signed up and we hope to get more! We need to select from our currently sign-up people to make them dodge ball players and from there we need to be able to select them to be on different teams. Please look over the Specs Checklist to make sure you understand the needs of this app.
 
 1. We have a players column, where we can move people to, from a people column. They they get 'player' values.
-2. Can add players to teams, such as the blue team. They also get the keys 'color' and 'mascot'
-3. Use 'Class' for new properties and use extend when needed.
+2. Can add players to teams, such as the blue or red team. They also get the properties 'color' and 'mascot', along with the other properties from player, via extend and super.
+3. Use 'Class' for new properties and use extend when needed. We will use class for Player and then extend to Red and Blue Teammate. 
 
-Need to create a class, that has canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience
-Then these are pushed to a new array, and displayed in the DOM
-Button will be added to select team for player
+Need to create a class, that has canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience.
+Then these are pushed to a new array, and displayed in the DOM.
+Button will be added to select team for player.
+We will need to create a blue and red team button. And then remove and apply the player to the appropriate team.
 
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-let assert = require('assert');
+// This is for testing. It's commented out so the DOM element will work. To verify the tests, just un-comment the line below.
+// const assert = require('assert');
 
-
+// Our array of people to choose from. These people will become players and join a team. They are held as objects.
 const arrOfPeople = [
     {
       id: 2,
@@ -55,10 +56,13 @@ const arrOfPeople = [
     },
   ]
   
+//   Variables set to empty arrays. To hold data.
   const listOfPlayers = []
   const blueTeam = []
   const redTeam = []
 
+
+// *** I started with a class Person, but I couldn't get it to extend correctly throughout the code.
 //   class Person {
 //       constructor(id, name, age, skillSet, placeBorn) {
 //           this.id = id;
@@ -69,6 +73,7 @@ const arrOfPeople = [
 //       }
 //   }
   
+// So I went with Player, and adding all the object properties in here. Then extended it to the teams.
   class Player {
     constructor(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
         this.id = id;
@@ -82,7 +87,7 @@ const arrOfPeople = [
         this.isHealthy = isHealthy;
         this.yearsExperience = yearsExperience;
     }
-
+// This pushes the class Player's properties into the arrays for 'blueTeam' and 'redTeam'.
     joinRedTeam(redTeam){
         this.redTeam = redTeam;
         player.redTeam.push(this);
@@ -94,7 +99,7 @@ const arrOfPeople = [
 }
 
 
-
+// Here we have a class of 'BlueTeammate' and 'RedTeammate'. It extends the 'Player' class and adds in 'color' and 'mascot'. We want to keep the properties from 'Player', hence we use 'super'.
 class BlueTeammate extends Player {
     constructor(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, color, mascot){
         super(id, name, age, skillSet, placeBorn, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience);
@@ -112,12 +117,14 @@ class RedTeammate extends Player{
 
 
 
-
+// This function lists the people from the array of objects at the beginning. Along with a 'make player' button.
 const listPeopleChoices = () => {
     const listElement = document.getElementById('people')
     arrOfPeople.map(person => {
     const li = document.createElement("li")
-    const button = document.createElement("button")
+    const button = document.createElement("button");
+    button.style.backgroundColor='green';
+    button.style.color = 'white';
     button.innerHTML = "Make Player"
     button.addEventListener('click', function() {makePlayer(person.id);
         listElement.removeChild(li);});
@@ -127,97 +134,110 @@ const listPeopleChoices = () => {
     });
     }
   
-  
+    //   This function makes the people into players! It ties into the HTML aspect. As the people are found, they are turned into players. They are given their properties, and if they 'canThrowBall' or 'hasPaid', etc, it's set to true.
   const makePlayer = (id) => {
     const players = document.getElementById('players');
-    const findPlayer = arrOfPeople.find(function(entry){  
+    const findPlayer = arrOfPeople.find((entry) => {  
       return entry.id == id;  
     });
     const playerIndex = arrOfPeople.indexOf(findPlayer);
     
-    const newPlayer = new Player (findPlayer.id, findPlayer.name, findPlayer.age, findPlayer.skillSet, findPlayer.placeBorn, true, true, true, true, 4);
+    const newPlayer = new Player (findPlayer.id, findPlayer.name, findPlayer.age, findPlayer.skillSet, findPlayer.placeBorn, true, true, true, true, 3);
     listOfPlayers.push(newPlayer);
     arrOfPeople.splice(playerIndex, 1);
-    
+
+    // We're making a list here. Of the new players.
     const li = document.createElement("li");
     li.appendChild(document.createTextNode(newPlayer.name + " - "));
 
+    // These are the buttons for the teams. Best practice would be to style it in CSS, but since it's a few things, I'll just put it here. Once these are clicked, that person, who became a player, is now on the 'red' or 'blue' team.
     const button_blue = document.createElement("button");
-    button_blue.style.backgroundColor='Blue';
-    button_blue.style.color = 'white';
-    button_blue.innerHTML = "Blue Team";
-    button_blue.addEventListener('click', function() {makeBluePlayer(newPlayer.id);
+    button_blue.style.backgroundColor='lightblue';
+    button_blue.style.color = 'blue';
+    button_blue.innerHTML = "Blue Team!";
+    button_blue.addEventListener('click', () => {makeBluePlayer(newPlayer.id);
       players.removeChild(li);})
     li.appendChild(button_blue);
+
     const button_red = document.createElement("button");
-    button_red.style.backgroundColor='Red';
-    button_red.style.color = 'yellow';
-    button_red.innerHTML = "Red Team";
-    button_red.addEventListener('click', function() {makeRedPlayer(newPlayer.id);
+    button_red.style.backgroundColor='white';
+    button_red.style.color = 'red';
+    button_red.innerHTML = "Red Team!";
+    button_red.addEventListener('click', () => {makeRedPlayer(newPlayer.id);
       players.removeChild(li);})
     li.appendChild(button_red);
 
     players.append(li);
     }
 
+    // Once they've been selected as a blue or red player. They are put into their respective sections. And are given the properties of a teammate. Along with their team's color and mascot.
     const makeBluePlayer = (id) =>{
       const playersBlue = document.getElementById('blue');
-      playersBlue.style.backgroundColor = 'blue';
-      playersBlue.style.color = 'white';
-      const selectBlue = listOfPlayers.find(function(pick){  
+      playersBlue.style.backgroundColor = 'white';
+      playersBlue.style.color = 'blue';
+      const selectBlue = listOfPlayers.find((pick) => {  
         return pick.id == id;  
       });
       const blueIndex = listOfPlayers.indexOf(selectBlue);
 
-      const newBluePlayer = new BlueTeammate (selectBlue.id, selectBlue.name, selectBlue.age, selectBlue.skillSet, selectBlue.placeBorn, true, true, true, true, 4, 'Blue', 'Tiger');
+      const newBluePlayer = new BlueTeammate (selectBlue.id, selectBlue.name, selectBlue.age, selectBlue.skillSet, selectBlue.placeBorn, true, true, true, true, 3, 'Blue', 'Tigers');
       blueTeam.push(newBluePlayer);
       listOfPlayers.splice(blueIndex, 1); 
 
       const li = document.createElement("li");
-      li.appendChild(document.createTextNode(newBluePlayer.name + " - " + "Mascot = " + newBluePlayer.mascot + " and Team Color is " + newBluePlayer.color));
+      li.appendChild(document.createTextNode(newBluePlayer.name + " - " + "is on the " + newBluePlayer.color + " Team, " + " GO " + newBluePlayer.mascot + "!"));
       playersBlue.append(li);
     }
+
+    // Here we make a red player. Very similar to making a blue player. Mascot and color of team is defined. Along with the statement after they've joined the team.
     const makeRedPlayer = (id) =>{
       const playersRed = document.getElementById('red');
-      playersRed.style.backgroundColor = 'red';
-      playersRed.style.color = 'yellow';
-      const selectRed = listOfPlayers.find(function(pick){
+      playersRed.style.backgroundColor = 'white';
+      playersRed.style.color = 'red';
+      const selectRed = listOfPlayers.find((pick) => {
         return pick.id == id;
       });
       const redIndex = listOfPlayers.indexOf(selectRed);
 
-      const newRedPlayer = new RedTeammate (selectRed.id, selectRed.name, selectRed.age, selectRed.skillSet, selectRed.placeBorn, true, true, true, true, 4, 'Red', 'Fish');
+      const newRedPlayer = new RedTeammate (selectRed.id, selectRed.name, selectRed.age, selectRed.skillSet, selectRed.placeBorn, true, true, true, true, 3, 'Red', 'Fishes');
       redTeam.push(newRedPlayer);
       listOfPlayers.splice(redIndex, 1);
       const li = document.createElement("li");
-      li.appendChild(document.createTextNode(newRedPlayer.name + " - " + "Mascot = " + newRedPlayer.mascot + " and Team Color is " + newRedPlayer.color));
+      li.appendChild(document.createTextNode(newRedPlayer.name + " - " + "is on the " + newRedPlayer.color + " Team, " + " GO " + newRedPlayer.mascot + "!"));
       playersRed.append(li);
     }
 
 
-    // Testing!
+    // Testing! Here are my tests, to ensure a newPlayer can be added from the people list. And then that they can join a team. And then that they have their team's color. Don't foget to un-comment the line near the beginning of the program, and also npm i.
+    
 
     if (typeof describe === 'function'){
-        describe('Player', function(){
-          it('should throw a ball, dodge a ball, has paid dues, is healthy, has experience', function(){
-            let newPlayer = new Player('newPlayer.id', 'newPlayer.name', 'newPlayer.age', 'newPlayer.skillSet', 'newPlayer.placeBorn', true, true, true, true, 4);
-            
-          });
+
+        describe('newPlayer', () => {
+          it('should make a player from the person list', () => {
+              let newPlayer = new Player(true, 'newPlayer.name');
+                assert.equal(newPlayer.id, true);
+            });
         });
-      }
-      if (typeof describe === 'function'){
-        describe('Team', function(){
-        it('team mate should have mascot', function(){
-          let  newBluePlayer = new BlueTeammate ('newBluePlayer.id', 'newBluePlayer.name', 'newBluePlayer.age', 'newBluePlayer.skillSet', 'newBluePlayer.placeBorn', true, true, true, true, 4, 'Dragon', 'Blue');
-          
+      
+      
+        describe('JoinTeam', () => {
+            it('Player joins a team', () => {
+                let  newBluePlayer = new BlueTeammate(true, 'newBluePlayer.name');
+                assert.equal(newBluePlayer.id, true);
+        });
+            it('Player joins a team', () => {
+                let  newRedPlayer = new RedTeammate(true, 'newRedPlayer.name');
+                assert.equal(newRedPlayer.id, true);
         });
       });
-    }
-        if (typeof describe === 'function'){
-          describe('Team', function(){
-          it('team mate should have team color', function(){
-            let  newBluePlayer = new BlueTeammate ('newBluePlayer.id', 'newBluePlayer.name', 'newBluePlayer.age', 'newBluePlayer.skillSet', 'newBluePlayer.placeBorn', true, true, true, true, 4, 'Dragon', 'Blue');
-            
+    
+        
+        describe('ColorOfTeam', () => {
+          it('Add color to teammate', () => {
+            let  newBluePlayer = new BlueTeammate(true, true, true, true, true, true, true, true, true, true, 'blue', true);
+            assert.equal(newBluePlayer.color, 'blue');
           });
       });
+
     }
